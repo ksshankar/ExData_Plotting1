4ae25ee10291b2fa2f5bb4f3101b0d7c0f224b5e
ksshankar <ksshankar1996@gmail.com>
2021-06-11 23:04:45 +0530

    Plot 4

--- plot4.R
+++ plot4.R
@@ -19,7 +19,7 @@ with(data,{
         plot(datetime,Sub_metering_1,col="black",type = "l",xlab="",ylab="Energy sub metering")
         lines(datetime,Sub_metering_2,col="red")
         lines(datetime,Sub_metering_3,col="blue")
-        legend("topright",col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,lwd=2)
+        legend("topright",col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,lwd=2,bty="n")
         plot(datetime,Global_reactive_power,type="l")
 })
 dev.off()
